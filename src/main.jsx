562855ebcb28fdd9621b1345b7e2e42ee9fb953e
nurxrdhn <ramadhannur148@gmail.com>
2026-08-10 12:10:07 +0700

    Susun kartu materi dua kolom

--- src/main.jsx
+++ src/main.jsx
@@ -24,6 +24,7 @@ import "./styles/experience-v8-20.css";
 import "./styles/experience-v8-23.css";
 import "./styles/experience-v8-24.css";
 import "./styles/experience-v8-25.css";
+import "./styles/experience-v8-26.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
