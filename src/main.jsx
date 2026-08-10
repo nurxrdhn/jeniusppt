70ce6fdd04b769fcd3e2499bcc55b7f5108c9111
Nur Ramadhan <ramadhannur148@gmail.com>
2026-08-10 23:50:33 +0700

    Perbarui background font dan warna slide

--- src/main.jsx
+++ src/main.jsx
@@ -27,6 +27,7 @@ import "./styles/experience-v8-25.css";
 import "./styles/experience-v8-26.css";
 import "./styles/experience-v8-27.css";
 import "./styles/experience-v8-28.css";
+import "./styles/experience-v8-29.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
