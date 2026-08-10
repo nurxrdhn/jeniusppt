ce1ed0629f1a1953060f22d1746f6134f2ae7703
nurxrdhn <ramadhannur148@gmail.com>
2026-08-10 09:25:20 +0700

    Perkecil kartu materi desktop

--- src/main.jsx
+++ src/main.jsx
@@ -21,6 +21,7 @@ import "./styles/experience-v8-16.css";
 import "./styles/experience-v8-17.css";
 import "./styles/experience-v8-19.css";
 import "./styles/experience-v8-20.css";
+import "./styles/experience-v8-23.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
